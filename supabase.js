4c2bdfd3d23c8e729fd51e0e138448a7af631617
yewin5375 <yewin5375@gmail.com>
2026-01-02 23:13:47 +0630

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,10 @@
+// js/supabase.js
+const { createClient } = supabase;
+
+const SB_URL = "https://iogpnvljnhsxuzzdltpb.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ3BudmxqbmhzeHV6emRsdHBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NTk2MDYsImV4cCI6MjA4MjMzNTYwNn0.Kxy3QW1gq-J-tzUF2fxQd0l989Wr7BI2uRTbWDTNL6k";
+
+// window.sb လို့ သုံးမှ တခြား file တွေက လှမ်းသုံးလို့ရမှာပါ
+window.sb = createClient(SB_URL, SB_KEY);
+
+console.log("Supabase Client Initialized!");
