d0c5457c53c20e00cd39d94991b3ceec4ba64368
yewin5375 <yewin5375@gmail.com>
2026-01-02 22:23:58 +0630

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
 // Supabase Connection
-const SB_URL = "https://YOUR_PROJECT_ID.supabase.co";
-const SB_KEY = "YOUR_PUBLIC_ANON_KEY";
+const SB_URL = "https://iogpnvljnhsxuzzdltpb.supabase.co";
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ3BudmxqbmhzeHV6emRsdHBiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2Njc1OTYwNiwiZXhwIjoyMDgyMzM1NjA2fQ.rNXHHO8PKO58ZXR4Eqzi6Bl0vzCIwH24jiwOVwbVPoc";
 
 const sb = supabase.createClient(SB_URL, SB_KEY);
