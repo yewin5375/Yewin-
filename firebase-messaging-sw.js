9f9ba9b0347d12d28eb6d71ab3ec6d3e3465ed6c
yewin5375 <yewin5375@gmail.com>
2026-01-02 23:45:59 +0630

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCW_jACdwWmN2nwlEOxR6tdTBqLXHyvE0w",
+  authDomain: "myin-thar-chicken-bbq.firebaseapp.com",
+  projectId: "myin-thar-chicken-bbq",
+  storageBucket: "myin-thar-chicken-bbq.firebasestorage.app",
+  messagingSenderId: "45639130854",
+  appId: "1:45639130854:web:779ecef328580d10e9e527"
+});
+
+const messaging = firebase.messaging();
