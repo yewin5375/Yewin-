71283490415f4cb4d074c6b4e885459fb1cd3284
yewin5375 <yewin5375@gmail.com>
2026-01-04 16:55:30 +0630

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,5 +2,5 @@ const { createClient } = supabase;
 const SB_URL = "https://iogpnvljnhsxuzzdltpb.supabase.co";
 const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ3BudmxqbmhzeHV6emRsdHBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3NTk2MDYsImV4cCI6MjA4MjMzNTYwNn0.Kxy3QW1gq-J-tzUF2fxQd0l989Wr7BI2uRTbWDTNL6k";
 
-window.sb = createClient(SB_URL, SB_KEY);
+window.supabase = createClient(SB_URL, SB_KEY);
 
